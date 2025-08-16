f622f2385e6b92ae8fb9b1967a8a9aa62ed978b9
BingHrrr <hrrrWang@gmail.com>
2025-08-16 22:34:02 +0800

    add user management module

--- src/main/java/com/kyle/kyleaicodegen/KyleAiCodeGenApplication.java
+++ src/main/java/com/kyle/kyleaicodegen/KyleAiCodeGenApplication.java
@@ -1,13 +1,13 @@
 package com.kyle.kyleaicodegen;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.kyle.kyleaicodegen.mapper")
 @SpringBootApplication
 public class KyleAiCodeGenApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KyleAiCodeGenApplication.class, args);
 	}
-
 }
